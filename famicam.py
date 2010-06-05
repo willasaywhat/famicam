7657e11e05e1618336c37781004045321da5b429
Abyss Knight [eee] <abyss@abyssknight.com>
2010-06-04 22:12:31 -0400

    Added changes by tetsu. Laughing man now replaces faces. Known issue with blue tint.

--- famicam.py
+++ famicam.py
@@ -6,6 +6,8 @@ import os
 import pycurl
 import twitter
 import imgur
+from pygame.transform import scale
+from pygame import image
 
 # Settings
 camera_ip = ""
@@ -13,39 +15,59 @@ haar_file = "" # Ex: "C:\OpenCV2.0\data\haarcascades\haarcascade_frontalface_def
 twitter_user = ""
 twitter_pass = ""
 imgur_api_key = ""
+WIDTH = 640
+HEIGHT = 480
+
+# Common Objects
+hc = cv.Load(haar_file)
+laughingman = image.load('laughing_man.png')
+stamp = time.strftime("%y%d%m%H%M%S",time.localtime())
 
 # Loop over the views
-views = ["9ff,4ff,24","6ff,4ff,24","0,2ff,24"]
+views = ["9ff,4ff,24","6ff,4ff,24","3ff,4ff,24", "ff,2ff,24"]
 for index, v in enumerate(views):
-	# Pan the camera to the view
+    # Pan the camera to the view
     params = urllib.urlencode({'AbsolutePanTilt': v})
     f = urllib.urlopen("http://"+camera_ip+"/command/ptzf.cgi", params)
     f.read()
-    stamp = time.strftime("%y%d%m%H%M%S",time.localtime())
+    
+    # Collect image (color, and black and white versions)
     source = os.path.join('./', stamp + '.jpg') 
     data = urllib.urlretrieve("http://"+camera_ip+"/oneshotimage.jpg", source)
-    hc = cv.Load(haar_file)
-    img = cv.LoadImage(source, 0)
-    faces = cv.HaarDetectObjects(img, hc, cv.CreateMemStorage())
-    for (x,y,w,h),n in faces:
-        cv.Rectangle(img, (x,y), (x+w,y+h), 255)
-    cv.SaveImage(source, img)
+    bwimg = cv.LoadImage(source, 0)
+    img = cv.LoadImage(source)
+
+    # Create pygame surfaces for bitblitting
+    pygameimg = img.tostring()
+    with_laughingman = image.fromstring(pygameimg, (WIDTH,HEIGHT), "RGB")
+    
+    # Detect faces and bitblit the laughingman
+    faces = cv.HaarDetectObjects(bwimg, hc, cv.CreateMemStorage())
+    for (fx,fy,fw,fh),n in faces:
+        (x,y,w,h) = (fx-10, fy-10, fw+20, fh+20)
+	laughingman_scaled = scale(laughingman, (w,h))
+        with_laughingman.blit(laughingman_scaled, (x,y))
+
+    # Save the image
+    image.save(with_laughingman, source)
 
     tweet = "Total people found in view ("+str(index+1)+"): "+str(len(faces))
 
-    print tweet
+    print tweet	
 
+
+    # Check if the room is dark
     hist = cv.CreateHist([32], cv.CV_HIST_ARRAY, [[0, 255]], 1)
 
-    cv.CalcHist([img], hist, 0, None)
+    cv.CalcHist([bwimg], hist, 0, None)
     hist_img = cv.CreateImage((32*10,255), 8, 3)
     (_,max_value,_,_) = cv.GetMinMaxHistValue(hist)
 
-    (sizex, sizey) = cv.GetSize(img)
+    (sizex, sizey) = cv.GetSize(bwimg)
 
     darkcount = 0
     for i in range(8):
-        darkcount = darkcount + cv.QueryHistValue_1D(hist, i)
+       darkcount = darkcount + cv.QueryHistValue_1D(hist, i)
 
     if darkcount > (.7 * (sizex * sizey)):
         print "Image is dark. Not sending data."
@@ -63,4 +85,3 @@ for index, v in enumerate(views):
 				
     # Clean up our mess
     os.remove(source)
-
